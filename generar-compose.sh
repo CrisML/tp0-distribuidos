cc2d1c267cd9e940a99641b0df7812ba942a4d44
CrisML <clin@fi.uba.ar>
2026-03-19 21:23:26 -0300

    fix: fix script for failing tests

--- generar-compose.sh
+++ generar-compose.sh
@@ -23,7 +23,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     volumes:
       - ./server/config.ini:/config.ini
     networks:
@@ -39,7 +38,6 @@ for i in $(seq 1 "$N"); do
     entrypoint: /client
     environment:
       - CLI_ID=${i}
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
